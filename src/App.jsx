118f6a9228d753e63b8979946fc47e7c3bb74d90
Rafael Fernandes <rafaelsf013@gmail.com>
2023-07-03 08:53:32 -0300

    feat: Add auth method

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import googleButton from './assets/google_signin_buttons/web/1x/btn_google_signi
 
 const navigate = url => window.location.href = url
 
+const auth = async () =>{
+
+  const response = await fetch('http://127.0.0.1:3000/request', {method: "post"})
+    
+  const data = await response.json()
+  navigate(data.url)
+
+}
 
 
 function App() {
