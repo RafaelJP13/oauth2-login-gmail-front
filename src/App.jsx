f3b7009fce6c7390b61950c2bf31b120e89cbba5
Rafael Fernandes <rafaelsf013@gmail.com>
2023-07-03 08:53:08 -0300

    feat: Add navigate method

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import googleButton from './assets/google_signin_buttons/web/1x/btn_google_signin_dark_pressed_web.png'
 
 
+const navigate = url => window.location.href = url
 
 
 
